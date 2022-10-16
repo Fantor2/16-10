dcb1ef74bcb6048e8050dd15ae6b606be5289e4f
Fantor2 <113799893+Fantor2@users.noreply.github.com>
2022-10-16 13:23:07 +0300

    Add files via upload

---
+++ 16 10.py
@@ -0,0 +1,91 @@
+##for i in range(1,6):
+##    print(i)
+##for i in range(5,0,-1):
+##    print(i)
+##for i in range(1,11):
+##    print('3 *',i,'=',3* i)
+##s=0
+##i=int(input())
+##for i in range(1,i+1):
+##    s+=i
+##    print(s)
+##for i in range(10,21):
+##    i *= i
+##    print(i)
+##n=int(input())
+##for i in range(1,n):
+##    i +=n
+##print(i)
+##n=int(input())
+##for i in range(1,n,n):
+##print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input())
+if or i in range if if int(input())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
